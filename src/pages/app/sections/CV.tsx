c85fca709c090b57acaafc05e15e2613398d4e26
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-05-11 15:06:08 +0200

    *add* front / cv section

---
+++ src/pages/app/sections/CV.tsx
@@ -0,0 +1,21 @@
+export default function CV(){
+    return(
+        <section className="flex flex-col w-full min-h-[100vh] items-center py-[100px] text-[#000000]">
+            <div className="flex flex-row max-w-[1200px] w-full bg-amber-200">
+                <div className="flex flex-col w-[300px] shrink-0 grow-0 p-[25px] bg-white">
+                    aaaaaaaa
+                </div>
+                <div className="flex flex-col w-full shrink grow">
+                    <span>
+                        I'm Sophie Carter, a passionate Web Designer & Developer based in the dynamic city of Berlin, Germany. I blend creative design with precise technical execution to deliver outstanding digital experiences.
+                    </span>
+                    <hr/>
+                    <div className="flex flex-row wrap w-full">
+                        <span>Product Design</span><span>UI Design</span>
+                    </div>
+                    <hr/>
+                </div>
+            </div>
+        </section>
+    )
+}
