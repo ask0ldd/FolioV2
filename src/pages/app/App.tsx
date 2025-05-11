7ef829b119d492fff86d881223eb818d51ac045a
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-05-11 15:05:48 +0200

    *edit* front / main page update

--- src/pages/app/App.tsx
+++ src/pages/app/App.tsx
@@ -1,6 +1,12 @@
 import './App.css'
-import design1 from '../assets/design1.jpg'
+import design1 from '../../assets/design1.jpg'
 import { useEffect, useState } from 'react'
+import Carousel from '../../components/Carousel/Carousel'
+import CarouselItem from '../../components/Carousel/CarouselItem'
+import KeyPoints from './sections/KeyPoints'
+import CV from './sections/CV'
+
+const items = Array.from({ length: 8 }, () => ({ image: design1, title: "Portfoy", link: "" }))
 
 function App() {
 
@@ -11,9 +17,9 @@ function App() {
   }, [])
 
   return (
-    <div className='bg-[#E1E7EC] w-full min-h-[100vh]'>
-      <div className='flex flex-col w-full min-h-[100vh] items-center relative overflow-hidden'>
-        {<div className='opacity-55 absolute w-full h-full heroBGGradient z-0'>
+    <div className='flex flex-col bg-[#E1E7EC] w-full min-h-[100vh]'>
+      <div className='flex flex-col w-full min-h-[100vh] items-center relative'>
+        {<div className='opacity-100 absolute w-full h-full heroBGGradient min-h-[100vh] z-0 overflow-hidden blur-md'> {/* heroBGGradient */}
           <div className='ray1 z-0'></div><div className='ray2 z-0'></div><div className='ray3 z-0'></div><div className='ray4 z-0'></div><div className='ray5 z-0'></div>
         </div>}
         <header className={visible ? 'flex w-full justify-between h-[80px] translate-y-[0px] opacity-100 items-center max-w-[1280px] z-10 headerTransition' : 'flex w-full justify-between h-[80px] translate-y-[-100px] opacity-0 items-center max-w-[1280px] z-10 headerTransition'} >
@@ -39,14 +45,14 @@ function App() {
 
           </div>
         </header>
-        <section className='flex flex-col w-full pt-[60px] pb-[100px] items-center z-10'>
+        <section className='flex flex-col w-full pt-[60px] pb-[100px] items-center z-10 overflow-hidden'>
           <div className={'flex flex-row gap-x-[20px] items-center heroTextTransition' + (visible ? ' translate-x-[0px] opacity-100 delay1s' : ' translate-x-[-25px] opacity-0 delay1s')} >
             <div className='h-[88px] w-[86px] bg-neutral-400 rounded-[16px] greyShadow'>
             </div>
-            <span className='instrument text-[100px] w-fit leading-snug heroTextGradient'>I'm Cedric, UI Designer</span>
+            <h1 className='instrument text-[100px] w-fit leading-snug heroTextGradient'>I'm Cedric, UI Designer</h1>
           </div>
           <div className={'flex flex-row gap-x-[20px] items-center' + (visible ? ' translate-x-[0px] opacity-100 heroTextTransition delay1-5s' : ' translate-x-[-25px] opacity-0 heroTextTransition delay1-5s')}>
-            <span className='instrument text-[100px] w-fit leading-none heroTextGradient'>& Full Stack Developer</span>
+            <h1 className='instrument text-[100px] w-fit leading-none heroTextGradient'>& Full Stack Developer</h1>
             <div className='h-[88px] w-[86px] bg-neutral-400 rounded-[16px] greyShadow cloudIcon'>
             </div>
             <div className="h-[88px] w-[86px] bg-neutral-400 rounded-[16px] greyShadow moonIcon">
@@ -61,45 +67,23 @@ function App() {
               <svg width="13" height="12" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M12.125 0.999981V9.12473C12.1251 9.24841 12.0885 9.36934 12.0198 9.47221C11.9512 9.57508 11.8535 9.65526 11.7393 9.7026C11.625 9.74994 11.4993 9.76232 11.378 9.73817C11.2567 9.71401 11.1453 9.65441 11.0578 9.5669L7.43764 5.94592L1.9425 11.4418C1.82523 11.5591 1.66618 11.625 1.50033 11.625C1.33448 11.625 1.17543 11.5591 1.05815 11.4418C0.940883 11.3246 0.875 11.1655 0.875 10.9997C0.875 10.8338 0.940883 10.6748 1.05815 10.5575L6.55408 5.06236L2.9331 1.44216C2.84559 1.35475 2.78599 1.24334 2.76183 1.12204C2.73768 1.00074 2.75006 0.875005 2.7974 0.760744C2.84474 0.646484 2.92492 0.548839 3.02779 0.480172C3.13066 0.411505 3.25159 0.374903 3.37527 0.375H11.5C11.6658 0.375 11.8247 0.440846 11.9419 0.558053C12.0592 0.67526 12.125 0.834226 12.125 0.999981Z" fill="white"/>
               </svg>
-              Get Template
+              My Resume
             </button>
             <button className='greyButton font-semibold'>
               <svg width="17" height="14" viewBox="0 0 17 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M16.013 7.44192L10.268 13.187C10.1806 13.2745 10.0692 13.3341 9.94789 13.3583C9.82659 13.3825 9.70085 13.3702 9.58658 13.3228C9.47231 13.2755 9.37464 13.1954 9.30595 13.0925C9.23726 12.9897 9.20063 12.8687 9.2007 12.7451L9.20125 7.62477L1.4294 7.62532C1.26355 7.62532 1.10449 7.55944 0.987223 7.44217C0.869951 7.3249 0.804068 7.16584 0.804068 7C0.804068 6.83415 0.869951 6.67509 0.987223 6.55782C1.10449 6.44055 1.26355 6.37467 1.4294 6.37467L9.20125 6.37522L9.2007 1.25493C9.20063 1.13125 9.23726 1.01033 9.30595 0.907476C9.37464 0.804624 9.47231 0.724465 9.58658 0.677148C9.70085 0.62983 9.82659 0.617482 9.94789 0.641666C10.0692 0.665849 10.1806 0.725478 10.268 0.813002L16.013 6.55807C16.1302 6.67527 16.1961 6.83424 16.1961 7C16.1961 7.16575 16.1302 7.32472 16.013 7.44192Z" fill="#0E1C29"/>
               </svg>
-              See Projects
+              My Projects
             </button>
           </div>
 
-          <div className={'flex flex-row w-full h-[405px] mt-[50px] overflow-x-hidden gap-x-[16px] relative z-20' + (visible ? ' translate-y-[0px] opacity-100 heroTextTransition delay3s' : ' translate-y-[50px] opacity-0 heroTextTransition delay3s')}>
-            
-            <div className='flex flex-col shrink-0 w-[475px] opacity-100 h-full border-1 border-[#cBcDcE] hover:border-[#bBbDbE] rounded-[8px] pt-[16px] py-[15px] items-center hover:bg-[#DEE3E6] cursor-pointer'>
-              <img className='carousselImg' alt="project 1 design" src={design1}/>
-              <span className='mt-[15px] ml-[15px] self-start'>Portfoy</span>
-            </div>
-            
-            <div className='flex flex-col shrink-0 w-[475px] opacity-100 h-full border-1 border-[#cBcDcE] hover:border-[#bBbDbE] rounded-[8px] pt-[16px] py-[15px] items-center hover:bg-[#DEE3E6] cursor-pointer'>
-              <img className='carousselImg' alt="project 1 design" src={design1}/>
-              <span className='mt-[15px] ml-[15px] self-start'>Portfoy</span>
-            </div>
-            
-            <div className='flex flex-col shrink-0 w-[475px] opacity-100 h-full border-1 border-[#cBcDcE] hover:border-[#bBbDbE] rounded-[8px] pt-[16px] py-[15px] items-center hover:bg-[#DEE3E6] cursor-pointer'>
-              <img className='carousselImg' alt="project 1 design" src={design1}/>
-              <span className='mt-[15px] ml-[15px] self-start'>Portfoy</span>
-            </div>
-              
-            <div className='flex flex-col shrink-0 w-[475px] opacity-100 h-full border-1 border-[#cBcDcE] hover:border-[#bBbDbE] rounded-[8px] pt-[16px] py-[15px] items-center hover:bg-[#DEE3E6] cursor-pointer'>
-              <img className='carousselImg' alt="project 1 design" src={design1}/>
-              <span className='mt-[15px] ml-[15px] self-start'>Portfoy</span>
-            </div>
-              
-            <div className='flex flex-col shrink-0 w-[475px] opacity-100 h-full border-1 border-[#cBcDcE] hover:border-[#bBbDbE] rounded-[8px] pt-[16px] py-[15px] items-center hover:bg-[#DEE3E6] cursor-pointer'>
-              <img className='carousselImg' alt="project 1 design" src={design1}/>
-              <span className='mt-[15px] ml-[15px] self-start'>Portfoy</span>
-            </div>
+          <Carousel items={items} visible={visible} Renderer={CarouselItem}/>
 
-          </div>
         </section>
+
+        <KeyPoints/>
+
+        <CV/>
       </div>
     </div>
   )
