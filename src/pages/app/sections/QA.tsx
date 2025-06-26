6db183112474d0050baf05e4c9e0941803bf53e3
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-06-26 19:11:59 +0200

    *add* front / QA Pair component

--- src/pages/app/sections/QA.tsx
+++ src/pages/app/sections/QA.tsx
@@ -68,7 +68,8 @@ export default function QA(){
                     </button>
                 </div>
 
-                <div className="flex flex-col flex-[60%] shrink grow h-fit">
+                <div className="flex flex-col flex-[60%] shrink grow h-fit gap-y-[15px]">
+
                     <div className="flex flex-col bg-[#F6FBFF] rounded-[10px] px-[20px] whiteBlockShadow">
                         <div className="flex flex-row justify-between h-[48px] items-center">
                             <span className="text-[16px] text-[#0E1C29]">What services do you offer?</span>
@@ -80,8 +81,27 @@ export default function QA(){
                             </div>
                         <span className="text-[14px] text-[#0E1C29] font-normal pb-[15px]">I specialize in web design, branding, UI/UX, and Framer development, creating modern, user-friendly experiences tailored to your needs</span>
                     </div>
+
+                    <QAPair question={"What services do you offer?"} answer={"I specialize in web design, branding, UI/UX, and Framer development, creating modern, user-friendly experiences tailored to your needs"}/>
+
                 </div>
             </div>
         </section>
+    )
+}
+
+function QAPair({question, answer} : {question : string, answer : string}){
+    return(
+        <div className="flex flex-col bg-[#F6FBFF] rounded-[10px] px-[20px] whiteBlockShadow">
+            <div className="flex flex-row justify-between h-[48px] items-center">
+                <span className="text-[16px] text-[#0E1C29]">{question}</span>
+                <svg width={20} height={20} xmlns="http://www.w3.org/2000/svg" 
+                    viewBox="0 0 256 256" 
+                    focusable="false" 
+                    color="var(--token-6396e7f2-0645-4f69-9a36-80e94f8ee015, rgb(14, 28, 41))" 
+                ><path d="M213.66,101.66l-80,80a8,8,0,0,1-11.32,0l-80-80A8,8,0,0,1,53.66,90.34L128,164.69l74.34-74.35a8,8,0,0,1,11.32,11.32Z"></path></svg>
+                </div>
+            <span className="text-[14px] text-[#0E1C29] font-normal pb-[15px]">{answer}</span>
+        </div>
     )
 }
