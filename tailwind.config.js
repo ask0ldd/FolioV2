3729cff7d972d61b4380835a1c22153b1b4a50ba
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-11-03 19:00:38 +0100

    *edit* front / tailwind config : replacing default breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,27 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+import { fontFamily as _fontFamily } from 'tailwindcss/defaultTheme';
 
-module.exports = {
-  content: ["./src/**/*.{html,ts,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        'main-green': '#004746',
-      },
-      boxShadow: {
-        custom: '0px 4px 8px rgba(0,0,0,0.5)',
-      },
-      fontFamily: {
-        'dm-sans': ['DM Sans', ...defaultTheme.fontFamily.sans],
-        'inter': ['Inter', ...defaultTheme.fontFamily.sans],
-        'dancing': ['Dancing Script', 'cursive'],
-        'instrument': ['Instrument Serif', 'serif'],
-      },
+export const content = ["./src/**/*.{html,ts,tsx}"];
+export const theme = {
+  screens: {
+    xs: '30rem', // 480px
+    sm: '40rem', // 640px
+    md: '50.625rem', // 810px
+    lg: '64rem', // 1024px
+    xl: '75rem', // 1200px
+  },
+  extend: {
+    colors: {
+      'main-green': '#004746',
     },
-    screens: {
-      'xs': '480px',      // Extra small devices
-      'sm': '640px',      // Small devices
-      'md': '810px',      // Medium devices
-      'lg': '1024px',     // Large devices
-      'xl': '1200px',     // Extra large devices
+    boxShadow: {
+      custom: '0px 4px 8px rgba(0,0,0,0.5)',
+    },
+    fontFamily: {
+      'dm-sans': ['DM Sans', ..._fontFamily.sans],
+      'inter': ['Inter', ..._fontFamily.sans],
+      'dancing': ['Dancing Script', 'cursive'],
+      'instrument': ['Instrument Serif', 'serif'],
     },
   },
-  plugins: [],
 };
+export const plugins = [];
