9c7d41259c1cb1867ffd46271b06b64e88568628
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-11-05 19:15:53 +0100

    *edit* front / happy clients section : minor design adjustments

--- src/pages/app/sections/HappyClients.tsx
+++ src/pages/app/sections/HappyClients.tsx
@@ -10,8 +10,8 @@ export default function HappyClients(){
     return(
         <section className='flex flex-col w-full items-center py-[70px] md:py-[100px] px-[20px]'>
             <div className='flex px-[2px] py-[2px] bg-[#EFF8FF] rounded-full'>
-                <span className='flex flex-row gap-x-[10px] justify-center items-center h-[40px] bg-[#EFF8FF] border-1 border-[#D8DFE5] px-[30px] rounded-full'>
-                    <svg viewBox="0 0 256 256" width={18} height={18} style={{transform:'translateY(1px)'}} focusable="false" color="#0e1c29">
+                <span className='flex flex-row gap-x-[10px] text-[14px] justify-center items-center h-[40px] bg-[#EFF8FF] border-1 border-[#D8DFE5] px-[30px] rounded-full'>
+                    <svg viewBox="0 0 256 256" width={16} height={16} style={{transform:'translateY(1px)'}} focusable="false" color="#0e1c29">
                         <g color="#0e1c29">
                             <path d="M240,56V200a8,8,0,0,1-8,8H8a8,8,0,0,1,0-16H56V152a8,8,0,0,1,8-8h48V104a8,8,0,0,1,8-8h48V56a8,8,0,0,1,8-8h56A8,8,0,0,1,240,56Z"></path>
                         </g>
