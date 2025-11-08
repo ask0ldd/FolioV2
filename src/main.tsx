f2ccb26d236b27d222797460586a971bfb8dc5b9
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-11-08 21:46:47 +0100

    *edit* front / main page : navigation handled

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './pages/app/App.tsx'
+import MainRouter from './router/MainRouter.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MainRouter/>
   </StrictMode>,
 )
