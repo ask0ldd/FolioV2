dc6f4f565f8255d30304514c6d49b373c829ada1
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-11-11 20:05:06 +0100

    *add* front / summary section : base layout

---
+++ src/pages/project/sections/Summary.tsx
@@ -0,0 +1,30 @@
+function Summary(){
+    return(
+        <section className="flex flex-row w-full items-center py-[75px] md:py-[100px] px-[20px] md:px-0 max-w-[1280px]">
+            <div className="flex flex-col w-[50%] max-w-[50%]">
+                <h1>Portfoy</h1>
+                <span>Alter is a sleek Framer template built for AI-driven SaaS platforms to enhance marketing automation and customer engagement.</span>
+            </div>
+            <div className="flex flex-row flex-wrap w-[50%] max-w-[50%] gap-x-[20px] gap-y-[16px]">
+                <div className="flex flex-col max-w-[50%] gap-y-[8px]">
+                    <em>Services</em>
+                    <span>Web Design, Development, No-Code</span>
+                </div>
+                <div className="flex flex-col max-w-[50%] gap-y-[8px]">
+                    <em>Services</em>
+                    <span>Web Design, Development, No-Code</span>
+                </div>
+                <div className="flex flex-col max-w-[50%] gap-y-[8px]">
+                    <em>Services</em>
+                    <span>Web Design, Development, No-Code</span>
+                </div>
+                <div className="flex flex-col max-w-[50%] gap-y-[8px]">
+                    <em>Services</em>
+                    <span>Web Design, Development, No-Code</span>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Summary
