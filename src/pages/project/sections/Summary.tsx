a7dc354a09cef481af5d86051afc7e64ef081a0f
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-11-11 20:16:17 +0100

    *edit* front / summary : improved styling

--- src/pages/project/sections/Summary.tsx
+++ src/pages/project/sections/Summary.tsx
@@ -1,25 +1,28 @@
 function Summary(){
     return(
-        <section className="flex flex-row w-full items-center py-[75px] md:py-[100px] px-[20px] md:px-0 max-w-[1280px]">
-            <div className="flex flex-col w-[50%] max-w-[50%]">
-                <h1>Portfoy</h1>
-                <span>Alter is a sleek Framer template built for AI-driven SaaS platforms to enhance marketing automation and customer engagement.</span>
+        <section className="flex flex-row w-full items-center py-[75px] md:py-[100px] px-[20px] md:px-0 max-w-[1280px] gap-x-[120px]">
+            <div className="flex flex-col flex-1 max-w-[50%]">
+                <div className="flex flex-col gap-y-[20px]">
+                    <h1 className="dm-sans text-[36px] leading-[3rem] md:text-[44px] xl:text-[56px]">Portfoy</h1>
+                    <span>Alter is a sleek Framer template built for AI-driven SaaS platforms to enhance marketing automation and customer engagement.</span>
+                </div>
+
             </div>
-            <div className="flex flex-row flex-wrap w-[50%] max-w-[50%] gap-x-[20px] gap-y-[16px]">
+            <div className="flex flex-row flex-wrap flex-1 max-w-[50%] gap-x-[20px] gap-y-[40px]">
                 <div className="flex flex-col max-w-[50%] gap-y-[8px]">
-                    <em>Services</em>
+                    <em className="font-semibold">Services</em>
                     <span>Web Design, Development, No-Code</span>
                 </div>
                 <div className="flex flex-col max-w-[50%] gap-y-[8px]">
-                    <em>Services</em>
+                    <em className="font-semibold">Services</em>
                     <span>Web Design, Development, No-Code</span>
                 </div>
                 <div className="flex flex-col max-w-[50%] gap-y-[8px]">
-                    <em>Services</em>
+                    <em className="font-semibold">Services</em>
                     <span>Web Design, Development, No-Code</span>
                 </div>
                 <div className="flex flex-col max-w-[50%] gap-y-[8px]">
-                    <em>Services</em>
+                    <em className="font-semibold">Services</em>
                     <span>Web Design, Development, No-Code</span>
                 </div>
             </div>
