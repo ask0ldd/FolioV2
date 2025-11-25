d644b2048eb14056e3dfe75049020cce95a42b68
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-11-25 21:26:45 +0100

    *edit* front / QA section : clean up

--- src/pages/app/sections/QA.tsx
+++ src/pages/app/sections/QA.tsx
@@ -42,12 +42,6 @@ export default function QA(){
                         <QAPair onToggle={() => setActiveQ(idx)} active={activeQ == idx} question={question.question} answer={question.answer}/>
                     ))}
 
-                    {/*<QAPair onToggle={() => setActiveQ(0)} active={activeQ == 0} question={"What services do you offer?"} answer={"I specialize in web design, branding, UI/UX, and Framer development, creating modern, user-friendly experiences tailored to your needs."}/>
-                    <QAPair onToggle={() => setActiveQ(1)} active={activeQ == 1} question={"Do you provide revisions?"} answer={"Yes! I offer two free rounds of revisions to ensure the final design meets your vision perfectly. Additional revisions are available if needed."}/>
-                    <QAPair onToggle={() => setActiveQ(2)} active={activeQ == 2} question={"How do I start working with you??"} answer={"Simply reach out through my contact page, and we’ll discuss your project, goals, and timeline before getting started."}/>
-                    <QAPair onToggle={() => setActiveQ(3)} active={activeQ == 3} question={"What is your pricing structure?"} answer={"Pricing depends on the project scope. Contact me for a custom quote based on your needs and budget."}/>
-                    <QAPair onToggle={() => setActiveQ(4)} active={activeQ == 4} question={"How long does a project take?"} answer={"Timelines vary based on project complexity, but most designs take 1–3 weeks, while full websites take 3–6 weeks."}/>*/}
-
                 </div>
             </div>
         </section>
